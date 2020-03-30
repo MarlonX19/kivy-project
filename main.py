e53af2c961b0896e9ead01fbc6c7d9ba7af1e111
marlonx19 <marlon.englemam@gmail.com>
2020-03-30 16:14:35 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+
+class App(App):
+    def build(self):
+        return Button(text='Ola coroners', font-size=)
+
+test = App()
+
+test.run()
