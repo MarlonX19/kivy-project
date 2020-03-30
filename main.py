197f03d673bfa8d20cb07ad53dec274eaae93b34
marlonx19 <marlon.englemam@gmail.com>
2020-03-30 16:54:30 -0300

    set window size and changed background color

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+
+from kivy.config import Config
+Config.set('graphics', 'width', '350')
+Config.set('graphics', 'height', '600')
+
+from kivy.core.window import Window
+Window.clearcolor = (.9, .20, .17, 1)
 
 
 class App(App):
     def build(self):
-        return Button(text='Ola coroners', font-size=)
+        return Label(text='Hello world')
 
 test = App()
 
