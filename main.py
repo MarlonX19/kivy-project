f928d012039d01d0f91de4dab22b8b2da8816a3e
marlonx19 <marlon.englemam@gmail.com>
2020-04-01 19:56:53 -0300

    marlon changes

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
 from kivy.config import Config
 Config.set('graphics', 'width', '350')
 Config.set('graphics', 'height', '600')
 
 from kivy.core.window import Window
-Window.clearcolor = (.9, .20, .17, 1)
+Window.clearcolor = (.12, .20, .17, 1)
 
+
+class FirstLayout(BoxLayout):
+    pass
+    
 
 class App(App):
     def build(self):
-        return Label(text='Hello world')
+        return FirstLayout()
 
 test = App()
-
 test.run()
