c3ba1e3b9a62db8f58885f707b3b9c016eafcb0d
ElderSam <eldersamuel98@gmail.com>
2020-04-02 23:32:13 -0300

    melhorando telas

--- main.py
+++ main.py
@@ -1,124 +1,79 @@
-import kivy   
-from kivy.app import App  
-from kivy.lang import Builder   
-from kivy.uix.screenmanager import ScreenManager, Screen 
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
 
-from kivy.config import Config
-Config.set('graphics', 'width', '350')
-Config.set('graphics', 'height', '600')
-   
-Builder.load_string(""" 
-<MenuScreen>: 
-    BoxLayout: 
-        orientation: 'vertical'
-        GridLayout:
-            cols: 1
-            Button: 
-                text: "CADASTRAR CONSULTA" 
-                font_size: 25
-                background_color : 0, 0, 1, 1 
-                on_press: 
-                    root.manager.transition.direction = 'left' 
-                    root.manager.transition.duration = 0.3 
-                    root.manager.current = 'new_consult_screen' 
-            Button: 
-                text: "HISTÓRICO DE CONSULTAS" 
-                font_size: 25
-                background_color : 0, 0, 1, 1 
-                on_press: 
-                    root.manager.transition.direction = 'left' 
-                    root.manager.transition.duration = 0.3  
-                    root.manager.current = 'list_consults_screen' 
-            Button: 
-                text: "REAGENDAR CONSULTA" 
-                font_size: 25
-                background_color : 0, 0, 1, 1 
-                on_press: 
-                    root.manager.transition.direction = 'left' 
-                    root.manager.transition.duration = 0.3  
-                    root.manager.current = 'reschedule_consult_screen' 
-            Button: 
-                text: "CANCELAR CONSULTA" 
-                font_size: 25
-                background_color : 0, 0, 1, 1 
-                on_press: 
-                    root.manager.transition.direction = 'left' 
-                    root.manager.transition.duration = 0.3  
-                    root.manager.current = 'cancel_consult_screen' 
- 
-<NewConsultScreen>: 
-    GridLayout:
-        cols: 1
-        GridLayout:
-            size_hint_y: 0.6
-            cols: 2
-            Label:
-                text: 'Seu nome'
-                markup: True
-                shorten: True
-                ellipsis_options: {'color':(1,0.5,0.5,1),'underline':True}
-            TextInput:
-                height: self.minimum_height
-                multiline: False
-                id: txt_input
-        Button: 
-            text: "VOLTAR AO MENU" 
-            font_size: 25
-            background_color : 0, 0, 1, 1 
-            on_press: 
-                root.manager.transition.direction = 'right' 
-                root.manager.transition.duration = 0.3  
-                root.manager.current = 'menu_screen'
-
-<ListConsultsScreen>: 
-    GridLayout:
-        cols : 1
-        Button: 
-            text: "VOLTAR AO MENU" 
-            font_size: 25
-            background_color : 0, 0, 1, 1 
-            on_press: 
-                root.manager.transition.direction = 'right' 
-                root.manager.transition.duration = 0.3  
-                root.manager.current = 'menu_screen'
-
-<RescheduleConsultScreen>: 
-    GridLayout:
-        cols : 1
-        Button: 
-            text: "VOLTAR AO MENU" 
-            font_size: 25
-            background_color : 0, 0, 1, 1 
-            on_press: 
-                root.manager.transition.direction = 'right' 
-                root.manager.transition.duration = 0.3  
-                root.manager.current = 'menu_screen'
-
-<CancelConsultScreen>: 
-    GridLayout:
-        cols : 1
-        Button: 
-            text: "VOLTAR AO MENU" 
-            font_size: 25
-            background_color : 0, 0, 1, 1 
-            on_press: 
-                root.manager.transition.direction = 'right' 
-                root.manager.transition.duration = 0.3  
-                root.manager.current = 'menu_screen'
-
-""") 
-   
-
-class MenuScreen(Screen): 
+class Gerenciador(ScreenManager):
     pass
-   
+
+class MenuScreen(Screen):
+    pass
+
 class NewConsultScreen(Screen): 
-    pass
+    
+    def __init__(self, tarefas=[], **kwargs): #keywords arguments
+        super().__init__(**kwargs)
+        
+        
+    def addWidget(self):
+        texto = self.ids.texto.text #pega o que o usuário digitou
+        print('texto digitado: ' + texto)
+        # ListConsultsScreen.ids.box.add_widget(Consult(text=texto)) #adiciona na lista
+        AllConsults.addNewConsult(texto) #adiciona na lista
+        self.ids.texto.text = ''
+        
+class AllConsults(object):
+    
+     
+    def __init__(self, listConsults=['teste']): 
+        self.listConsults = listConsults
+        
+        
+    @staticmethod
+    def addNewConsult(texto=''):
+        
+        return AllConsults.addNew(texto)
+             
 
+    def addNew(self, texto=''):
+        
+        self.listConsults.append(texto)
+        return self.listConsults
+        
+    @staticmethod  
+    def getList():
+        pass
+      #  return AllConsults.getListConsults()
+    
+    
+    def getListConsults(self):
+        return self.listConsults
+        
+        
+    
 class ListConsultsScreen(Screen): 
-    pass
+    def __init__(self, consultas=[], **kwargs): #keywords arguments
+        super().__init__(**kwargs)
+        
+       # consultas = AllConsults.getList()
+        
+        for consulta in consultas:   
+            print('item: ' + consulta)
+            #self.ids.box.add_widget(Consult(text=consulta))
+            
+            
+    def on_pre_enter(self):
+        Window.bind(on_keyboard=self.voltar)
+       
+    def voltar(self, window, key, *args):
+        if key == 27:
+            App.get_running_app().root.current = 'menu'
+            return True
+        
+    def on_pre_leave(self):
+        Window.unbind(on_keyboard=self.voltar) #desativa a funcionalidade da tecla
+        
+        
   
 class RescheduleConsultScreen(Screen): 
     pass
@@ -127,19 +82,18 @@ class CancelConsultScreen(Screen):
     pass
    
 
-screen_manager = ScreenManager() 
-   
-screen_manager.add_widget(MenuScreen(name ="menu_screen")) 
-screen_manager.add_widget(NewConsultScreen(name ="new_consult_screen")) 
-screen_manager.add_widget(ListConsultsScreen(name ="list_consults_screen")) 
-screen_manager.add_widget(RescheduleConsultScreen(name ="reschedule_consult_screen")) 
-screen_manager.add_widget(CancelConsultScreen(name ="cancel_consult_screen")) 
+
+# classe Tarefa
+class Consult(BoxLayout):
+    def __init__(self, text='', **kwargs):
+        super().__init__(**kwargs)
+        self.ids.label.text = text
+
+      
+class Sample_app(App):
+    def build(self):
+        return Gerenciador() 
+    
   
 
-class ScreenApp(App): 
-    def build(self): 
-        return screen_manager 
-  
-
-sample_app = ScreenApp() 
-sample_app.run() 
+Sample_app().run()
