4e3332354bbfe605206c70a2ab4636061e5d7442
Eduardo Geraldini <eduardopr.geraldini@gmail.com>
2020-04-05 21:32:38 -0300

    lista de consultas

--- ListConsultsScreen.py
+++ ListConsultsScreen.py
@@ -1,20 +1,53 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.core.window import Window
 
+import sqlite3
+        
 class ListConsultsScreen(Screen): 
-    def __init__(self, consultas=[], **kwargs): #keywords arguments
-        super().__init__(**kwargs)
+    def __init__(self, **kwargs): #keywords arguments
+        super(ListConsultsScreen, self).__init__(**kwargs)
         
-       # consultas = AllConsults.getList()
-        
-        for consulta in consultas:   
-            print('item: ' + consulta)
-            #self.ids.box.add_widget(Consult(text=consulta))
+    def returnAllConsults(self):
+        try:
+            conn = sqlite3.connect('DBApp.db')
+            cur = conn.cursor()
+
+            cur.execute("SELECT id, description, date FROM consults")
+            rows = cur.fetchall()
             
-            
+            return rows
+        except Exception as e:
+            print(e)
+            conn.rollback()
+            return
+        finally:
+            conn.close()  
+
+    def removeConsult(self, idConsult):
+        print(idConsult)
+
+
     def on_pre_enter(self):
         Window.bind(on_keyboard=self.voltar)
+
+        consults = self.returnAllConsults()
+
+        for consult in consults:
+            if consult[1] != None and consult[2] != None: 
+                self.ids.boxlist.add_widget(Label(text='Descrição: ' + consult[1] + '\n' + 'Consulta em: ' + consult[2], 
+                                                  font_size=15, 
+                                                  size_hint_y=None, 
+                                                  height=100))  
+                self.ids.boxlist.add_widget(Button(text='Excluir Consulta', 
+                                font_size=15, 
+                                size_hint_y=None, 
+                                height=40,
+                                on_press=lambda x: self.removeConsult(consult[0])))
+
        
     def voltar(self, window, key, *args):
         if key == 27:
